3a6468f0d9ff4647884c0f620c5a47ac7fd06c47
Hussein  El Amouri <46263751+hussein88al88amouri@users.noreply.github.com>
2023-12-05 14:31:07 +0100

    Update CopKmeans.py

--- bin/CopKmeans.py
+++ bin/CopKmeans.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
-#%%
+"""AI is creating summary for
+
+Raises:
+    Exception: [description]
+
+Returns:
+    [type]: [description]
+"""
+
 import random
 import numpy as np
-from tslearn.barycenters import  dtw_barycenter_averaging as dtwDBA
+from tslearn.barycenters import dtw_barycenter_averaging as dtwDBA
 from tslearn.metrics import dtw
+from scipy.spatial.distance import cdist
 
-#DTW  is adapted to multi dimentional, Euclidean to do
-def cop_kmeans(dataset, k, ml=[], cl=[], metric='l2_distance', type_='dependent',
-               initialization='kmpp',
-               max_iter=100, tol=1e-4):
+
+# DTW  is adapted to multi dimentional, Euclidean to do
+def cop_kmeans(dataset, k, ml=[], cl=[],
+               metric='l2_distance', type_='dependent',
+               initialization='kmpp', max_iter=100, tol=1e-4):
 
     ml, cl = transitive_closure(ml, cl, len(dataset))
-    ml_info = get_ml_info(ml, dataset,metric=metric)
+    ml_info = get_ml_info(ml,
+                          dataset,
+                          metric=metric)
     tol = tolerance(tol, dataset)
-    centers = initialize_centers(dataset, k, initialization,metric=metric, type_=type_)
+    centers = initialize_centers(dataset,
+                                 k,
+                                 initialization,
+                                 metric=metric,
+                                 type_=type_)
     centers = np.array(centers)
     iterr = 0
     for _ in range(max_iter):
@@ -21,7 +37,10 @@ def cop_kmeans(dataset, k, ml=[], cl=[], metric='l2_distance', type_='dependent'
         clusters_ = [-1] * len(dataset)
         clusters_ = np.array(clusters_)
         for i, d in enumerate(dataset):
-            indices, _ = closest_clusters(centers, dataset[[i]],metric=metric, type_=type_)
+            indices, _ = closest_clusters(centers,
+                                          dataset[[i]],
+                                          metric=metric,
+                                          type_=type_)
             counter = 0
             if clusters_[i] == -1:
                 found_cluster = False
@@ -35,7 +54,12 @@ def cop_kmeans(dataset, k, ml=[], cl=[], metric='l2_distance', type_='dependent'
                     counter += 1
                 if not found_cluster:
                     return None, None
-        clusters_, centers_  = compute_centers(clusters_, dataset,k, ml_info, metric=metric, type_=type_)
+        clusters_, centers_ = compute_centers(clusters_,
+                                              dataset,
+                                              k,
+                                              ml_info,
+                                              metric=metric,
+                                              type_=type_)
         # print(f'shift value {shift} tol value {tol}')
         # if shift <= tol:
         #     print(f'shift value {shift} tol value {tol}')
@@ -46,48 +70,86 @@ def cop_kmeans(dataset, k, ml=[], cl=[], metric='l2_distance', type_='dependent'
 
     return clusters_, centers_
 
-def dtw_distance(point1, point2,type_='dependent'):
-    #timestamps are the rows and the multivariate features are the columns
-    point1 = np.array(point1) if type(point1) != type(np.array([])) else point1
-    point2 = np.array(point2) if type(point2) != type(np.array([])) else point2
-    if type_ == 'dependent': 
-        return dtw(point1[0],point2[0])
+
+def dtw_distance(point1, point2, type_='dependent'):
+
+    # timestamps are the rows and the multivariate features are the columns
+    point1 = np.array(point1) if not isinstance(point1, np.ndarray) else point1
+    point2 = np.array(point2) if not isinstance(point2, np.ndarray) else point2
+    if type_ == 'dependent':
+        return dtw(point1[0], point2[0])
     elif type_ == 'independent':
         dtwdistance = 0
         for dim in range(point1.shape[2]):
-            x = point1[0,:,dim]
-            y = point2[0,:,dim]
-            dtwdistance = dtwdistance +  dtw(x,y)          
+            x = point1[0, :, dim]
+            y = point2[0, :, dim]
+            dtwdistance = dtwdistance + dtw(x, y)
         return dtwdistance
-    
+
+
 def l2_distance(point1, point2):
-        from scipy.spatial.distance import cdist
-        return cdist(point1[0, :, :].reshape((1,-1)), point2[0, :, :].reshape((1,-1)),'euclidean')[0][0]**2
-        return np.linalg.norm(point1[0, :, :] - point2[0, :, :])**2
-        summ = 0
-        for ii in range(point1.shape[2]):
-            summ += sum([(float(i)-float(j))**2 for (i, j) in zip(point1[0,:,ii], point2[0,:,ii])])
-        return np.sqrt(summ)
+    return cdist(point1[0, :, :].reshape((1, -1)),
+                 point2[0, :, :].reshape((1, -1)),
+                 'euclidean')[0][0] ** 2
+    # return np.linalg.norm(point1[0, :, :] - point2[0, :, :])**2
+    # summ = 0
+    # for ii in range(point1.shape[2]):
+    #     summ += sum([(float(i)-float(j))**2 for (i, j) in zip(point1[0,:,ii], point2[0,:,ii])])
+    # return np.sqrt(summ)
+
 
 # taken from scikit-learn (https://goo.gl/1RYPP5)
 def tolerance(tol, dataset):
     n = len(dataset)
     tsLenght = len(dataset[0])
-    averages = [sum(dataset[i][d] for i in range(n))/float(n) for d in range(tsLenght)]
-    variances = [sum((dataset[i][d]-averages[d])**2 for i in range(n))/float(n) for d in range(tsLenght)]
+    averages = [sum(dataset[i][d] for i in range(n))/float(n)
+                for d in range(tsLenght)]
+    variances = [sum((dataset[i][d] - averages[d]) ** 2
+                     for i in range(n))/float(n)
+                 for d in range(tsLenght)]
     return np.mean(tol * sum(variances) / tsLenght)
 
-def closest_clusters(centers, datapoint, metric='l2_distance', type_='dependent'):
+
+def closest_clusters(centers, datapoint,
+                     metric='l2_distance', type_='dependent'):
+    """AI is creating summary for closest_clusters
+
+    Args:
+        centers ([type]): [description]
+        datapoint ([type]): [description]
+        metric (str, optional): [description]. Defaults to 'l2_distance'.
+        type_ (str, optional): [description]. Defaults to 'dependent'.
+
+    Returns:
+        [type]: [description]
+    """
     if metric == 'l2_distance':
-        distances = [l2_distance(centers[[center]], datapoint) for
-                     center in range(len(centers))]
+        distances = [l2_distance(centers[[center]],
+                                 datapoint)
+                     for center in range(len(centers))]
     elif metric == 'dtw_distance':
-        distances = [dtw_distance(centers[[center]], datapoint, type_=type_) for
-                     center in range(len(centers))]
-        
+        distances = [dtw_distance(centers[[center]],
+                                  datapoint,
+                                  type_=type_)
+                     for center in range(len(centers))]
+
     return sorted(range(len(distances)), key=lambda x: distances[x]), distances
 
-def initialize_centers(dataset, k, method,metric='l2_distance', type_='dependent'):
+
+def initialize_centers(dataset, k, method,
+                       metric='l2_distance', type_='dependent'):
+    """AI is creating summary for initialize_centers
+
+    Args:
+        dataset ([type]): [description]
+        k ([type]): [description]
+        method ([type]): [description]
+        metric (str, optional): [description]. Defaults to 'l2_distance'.
+        type_ (str, optional): [description]. Defaults to 'dependent'.
+
+    Returns:
+        [type]: [description]
+    """
     if method == 'random':
         ids = list(range(len(dataset)))
         random.shuffle(ids)
@@ -111,7 +173,11 @@ def initialize_centers(dataset, k, method,metric='l2_distance', type_='dependent
                 tsLength = dataset.shape[1]
                 dim = dataset.shape[2]
                 for index, point in enumerate(dataset):
-                    cids, distances = closest_clusters(centers_arr, point.reshape(-1,tsLength,dim),metric=metric, type_=type_)
+                    point = point.reshape(-1, tsLength, dim)
+                    cids, distances = closest_clusters(centers_arr,
+                                                       point,
+                                                       metric=metric,
+                                                       type_=type_)
                     chances[index] = distances[cids[0]]
 
             elif metric == 'dtw_distance':
@@ -119,13 +185,30 @@ def initialize_centers(dataset, k, method,metric='l2_distance', type_='dependent
                 tsLength = dataset.shape[1]
                 dim = dataset.shape[2]
                 for index, point in enumerate(dataset):
-                    cids, distances = closest_clusters(centers_arr, point.reshape(-1,tsLength,dim),metric=metric, type_=type_)
+                    point = point.reshape(-1, tsLength, dim)
+                    cids, distances = closest_clusters(centers_arr,
+                                                       point,
+                                                       metric=metric,
+                                                       type_=type_)
                     chances[index] = distances[cids[0]]
-                
+
             kk = kk + 1
         return centers
 
+
 def violate_constraints(data_index, cluster_index, clusters, ml, cl):
+    """AI is creating summary for violate_constraints
+
+    Args:
+        data_index ([type]): [description]
+        cluster_index ([type]): [description]
+        clusters ([type]): [description]
+        ml ([type]): [description]
+        cl ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     for i in ml[data_index]:
         if clusters[i] != -1 and clusters[i] != cluster_index:
             return True
@@ -136,7 +219,23 @@ def violate_constraints(data_index, cluster_index, clusters, ml, cl):
 
     return False
 
-def compute_centers(clusters, dataset, k, ml_info,metric='l2_distance', type_='dependent'):
+
+def compute_centers(clusters, dataset, k, ml_info,
+                    metric='l2_distance', type_='dependent'):
+    """AI is creating summary for compute_centers
+
+    Args:
+        clusters ([type]): [description]
+        dataset ([type]): [description]
+        k ([type]): [description]
+        ml_info ([type]): [description]
+        metric (str, optional): [description]. Defaults to 'l2_distance'.
+        type_ (str, optional): [description]. Defaults to 'dependent'.
+
+    Returns:
+        [type]: [description]
+    """
+
     cluster_ids = set(clusters)
     k_new = len(cluster_ids)
     id_map = dict(zip(cluster_ids, range(k_new)))
@@ -148,13 +247,13 @@ def compute_centers(clusters, dataset, k, ml_info,metric='l2_distance', type_='d
     counts = [0] * k_new
     if metric == 'l2_distance':
         clusters_arr = np.array(clusters)
-        centers = np.zeros([k,tsLength,dim])
+        centers = np.zeros([k, tsLength, dim])
         for j, c in enumerate(clusters):
             for i in range(tsLength):
                 for d in range(dim):
                     centers[c][i][d] += dataset[j][i][d]
             counts[c] += 1
-    
+
         for j in range(k_new):
             for i in range(tsLength):
                 for d in range(dim):
@@ -162,17 +261,20 @@ def compute_centers(clusters, dataset, k, ml_info,metric='l2_distance', type_='d
 
     elif metric == 'dtw_distance':
         clusters_arr = np.array(clusters)
-        centers = np.zeros([k,tsLength,dim])
+        centers = np.zeros([k, tsLength, dim])
         for cluster_id in id_map.values():
             group = np.where(clusters_arr == cluster_id)[0]
-            centers[cluster_id] = dtwDBA(dataset[group].reshape(-1, tsLength, dim))
+            centers[cluster_id] = dtwDBA(dataset[group].reshape(-1,
+                                                                tsLength,
+                                                                dim))
             # counts[cluster_id] = group.shape[0]
 
     if k_new < k:
         ml_groups, ml_scores, ml_centroids = ml_info
 
         if metric == 'l2_distance':
-            current_scores = [sum(l2_distance(centers[[clusters[i]]], dataset[[i]])
+            current_scores = [sum(l2_distance(centers[[clusters[i]]],
+                                              dataset[[i]])
                                   for i in group)
                               for group in ml_groups]
             group_ids = sorted(range(len(ml_groups)),
@@ -180,7 +282,8 @@ def compute_centers(clusters, dataset, k, ml_info,metric='l2_distance', type_='d
                                reverse=True)
 
         elif metric == 'dtw_distance':
-            current_scores = [sum(dtw_distance(centers[[clusters[i]]], dataset[[i]], type_=type_)
+            current_scores = [sum(dtw_distance(centers[[clusters[i]]],
+                                               dataset[[i]], type_=type_)
                                   for i in group)
                               for group in ml_groups]
             group_ids = sorted(range(len(ml_groups)),
@@ -194,16 +297,24 @@ def compute_centers(clusters, dataset, k, ml_info,metric='l2_distance', type_='d
             for i in ml_groups[gid]:
                 clusters[i] = cid
 
-    if metric =='dtw_distance':
+    if metric == 'dtw_distance':
         return np.array(clusters), np.array(centers)
     else:
         return clusters, centers
-    
-def get_ml_info(ml, dataset,metric='l2_distance', type_='dependent'):
+
+
+def get_ml_info(ml, dataset, metric='l2_distance', type_='dependent'):
+    """AI is creating summary for
+
+    Returns:
+        [type]: [description]
+    """
+
     flags = [True] * len(dataset)
     groups = []
     for i in range(len(dataset)):
-        if not flags[i]: continue
+        if not flags[i]:
+            continue
         group = list(ml[i] | {i})
         groups.append(group)
         for j in group:
@@ -213,7 +324,8 @@ def get_ml_info(ml, dataset,metric='l2_distance', type_='dependent'):
     # using euclidean distance, need to be adapted for multivariate
     if metric == 'l2_distance':
 
-        nbcentroids, tsLength, dim = len(groups), dataset.shape[1], dataset.shape[2]
+        nbcentroids = len(groups)
+        tsLength, dim = dataset.shape[1], dataset.shape[2]
         centroids = np.zeros([nbcentroids, tsLength, dim])
         for j, group in enumerate(groups):
             for tsL in range(tsLength):
@@ -224,9 +336,10 @@ def get_ml_info(ml, dataset,metric='l2_distance', type_='dependent'):
         scores = [sum(l2_distance(centroids[[j]], dataset[[i]])
                       for i in groups[j])
                   for j in range(len(groups))]
-    
-    elif  metric == 'dtw_distance':
-        nbcentroids, tsLength, dim = len(groups), dataset.shape[1], dataset.shape[2]
+
+    elif metric == 'dtw_distance':
+        nbcentroids = len(groups)
+        tsLength, dim = dataset.shape[1], dataset.shape[2]
         centroids = np.zeros([nbcentroids, tsLength, dim])
         for j, group in enumerate(groups):
             centroids[j] = dtwDBA(dataset[group].reshape(-1, tsLength, dim))
@@ -236,7 +349,21 @@ def get_ml_info(ml, dataset,metric='l2_distance', type_='dependent'):
 
     return groups, scores, centroids
 
+
 def transitive_closure(ml, cl, n):
+    """AI is creating summary for transitive_closure
+
+    Args:
+        ml ([type]): [description]
+        cl ([type]): [description]
+        n ([type]): [description]
+
+    Raises:
+        Exception: [description]
+
+    Returns:
+        [type]: [description]
+    """
     ml_graph = dict()
     cl_graph = dict()
     for i in range(n):
@@ -278,7 +405,7 @@ def transitive_closure(ml, cl, n):
     for i in ml_graph:
         for j in ml_graph[i]:
             if j != i and j in cl_graph[i]:
-                raise Exception('inconsistent constraints between %d and %d' %(i, j))
+                raise Exception('inconsistent constraints between %d and %d'
+                                % (i, j))
 
     return ml_graph, cl_graph
-
